65f4b168f8aed3dbb299ca0b174d22faf211d92a
1313ou <1313ou@gmail.com>
2022-01-02 09:08:43 +0100

    script update

--- git_all.sh
+++ git_all.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+source define_colors.sh
+
 modules=$(find . -maxdepth 1 -type d -name 'oewn*' -printf '%P\n')
 
 for m in ${modules}; do 
-	echo "${m}"
+	echo -e "${Y}${m}${Z}"
 	pushd "${m}" > /dev/null
-	git status
+	git $*
 	echo
 	popd > /dev/null
 done
