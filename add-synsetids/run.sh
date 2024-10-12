37f2f57cddcb91ef34245bb889f9799792a63b32
1313ou <1313ou@gmail.com>
2024-10-12 10:37:41 +0200

    house keeping

--- add-synsetids/run.sh
+++ add-synsetids/run.sh
@@ -9,8 +9,8 @@ C='\u001b[36m'
 Z='\u001b[0m'
 
 DIRSRC="../oewn-grind_yaml2sql/sql"
-DB="oewn-2024-sqlite-2.1.1.sqlite"
-DBX="oewn-2024-sqlite-2.1.1-x.sqlite"
+DB="oewn-2024-sqlite-2.1.3.sqlite"
+DBX="oewn-2024-sqlite-2.1.3-x.sqlite"
 
 cp ${DIRSRC}/${DB} ${DIRSRC}/${DBX}
 python3 add_synsetids.py ${DIRSRC}/${DBX} synsets.map
