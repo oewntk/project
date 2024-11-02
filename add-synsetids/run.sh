43079835364df655c0881a0c54a23e02fd1aa793
1313ou <1313ou@gmail.com>
2024-11-02 21:11:03 +0100

    bew version 2.2.2

--- add-synsetids/run.sh
+++ add-synsetids/run.sh
@@ -9,8 +9,8 @@ C='\u001b[36m'
 Z='\u001b[0m'
 
 DIRSRC="../oewn-grind_yaml2sql/sql"
-DB="oewn-2024-sqlite-2.2.1.sqlite"
-DBX="oewn-2024-sqlite-2.2.1-synsetids.sqlite"
+DB="oewn-2024-sqlite-2.2.2.sqlite"
+DBX="oewn-2024-sqlite-2.2.2-synsetids.sqlite"
 
 cp ${DIRSRC}/${DB} ${DIRSRC}/${DBX}
 python3 add_synsetids.py ${DIRSRC}/${DBX} synsets.map
