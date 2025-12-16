f233d27335beb3c04836b86fab244c90c03cb69f
1313ou <1313ou@gmail.com>
2025-12-16 08:27:58 +0100

    bump new version

--- define_build.sh
+++ define_build.sh
@@ -4,9 +4,10 @@
 # Copyright (c) 2024. Bernard Bou.
 #
 
-export TAG=2024
+export TAG=2025
 export TAG31=31
 export BUILDMAJOR=2
 export BUILDMINOR=3
-export BUILDREV=1
+export BUILDREV=2
 export BUILD="${BUILDMAJOR}.${BUILDMINOR}.${BUILDREV}"
+# 2.3.2
