e02fec04be93784949aa9ac868ab7f979591a177
1313ou <1313ou@gmail.com>
2026-06-05 08:27:57 +0200

    adjust build number

--- define_build.sh
+++ define_build.sh
@@ -6,8 +6,8 @@
 
 export TAG=2026
 export TAG31=31
-export BUILDMAJOR=2
-export BUILDMINOR=3
-export BUILDREV=2
+export BUILDMAJOR=3
+export BUILDMINOR=0
+export BUILDREV=1
 export BUILD="${BUILDMAJOR}.${BUILDMINOR}.${BUILDREV}"
 # 3.0.1
