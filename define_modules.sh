0363f58fb798dc7137a836f34b6e6d9eaff24abc
1313ou <1313ou@gmail.com>
2026-06-06 11:56:29 +0200

    serialize lexid for senses

---
+++ define_modules.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2024. Bernard Bou.
+#
+
+export modules="
+.
+oewn-fromjson
+oewn-fromser
+oewn-fromwndb
+oewn-fromxml
+oewn-fromyaml
+oewn-grind
+oewn-grind_wndb2json
+oewn-grind_wndb2sql
+oewn-grind_wndb2wndb
+oewn-grind_xml2wndb
+oewn-grind_yaml2json
+oewn-grind_yaml2plus
+oewn-grind_yaml2ser
+oewn-grind_yaml2sql
+oewn-grind_yaml2wndb
+oewn-json
+oewn-model
+oewn-model-test
+oewn-parse_wndb
+oewn-tojson
+oewn-toser
+oewn-tosql
+oewn-towndb
+oewn-toyaml
+"
+
+echo $modules
---
+++ do_for_modules.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2024. Bernard Bou.
+#
+
+R='\u001b[31m'
+G='\u001b[32m'
+B='\u001b[34m'
+Y='\u001b[33m'
+M='\u001b[35m'
+C='\u001b[36m'
+Z='\u001b[0m'
+
+for m in $(.define_modules.sh); do
+  echo -e "${Y}${m}${Z}"
+  pushd $m > /dev/null
+  if ! eval "$1"; then
+    echo -e "${R}failed${Z}"
+    fi
+  popd > /dev/null
+done
