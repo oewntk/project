101130f658b7a94f10eb3ecc8bb6b4bce344f8c0
1313ou <1313ou@gmail.com>
2026-06-27 15:06:10 +0200

    house keeping

--- define_confirm.sh
+++ define_confirm.sh
@@ -4,23 +4,30 @@
 # Copyright (c) 2024. Bernard Bou.
 #
 
-# C O L O R S
-
 R='\u001b[31m'
 G='\u001b[32m'
 B='\u001b[34m'
 Y='\u001b[33m'
 M='\u001b[35m'
 C='\u001b[36m'
+W='\u001b[37m'
 Z='\u001b[0m'
+E='\u001b[1m'
+ZE='\u001b[22m'
 
 function confirm() {
-    echo -e "Using the ${C}${1}${Z} branch."
-    read -p "Are you sure you want to use the ${1} branch ? " -n 1 -r
+    echo -en "${W}${E}${1}${Z}${ZE}"
+    read -p " ?> " -n 1 -r
     echo -e "${Z}"
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         return 0
     fi
     return 1
+}
+
+function confirm_or_exit() {
+    if ! confirm "${1}"; then
+        exit 1
+    fi
 }
 
