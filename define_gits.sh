48c58eb6341a66b82e3652c3f96723dc498b55a5
1313ou <1313ou@gmail.com>
2026-08-25 11:15:50 +0200

    align with dev

--- define_gits.sh
+++ define_gits.sh
@@ -4,5 +4,6 @@
 # Copyright (c) 2024. Bernard Bou.
 #
 
-export modules=$(find . -maxdepth 1 -type d -name 'oewn-*' -printf '%P\n')
+export modules1=$(find . -maxdepth 1 -type d -name 'oewn-*' -printf '%P\n')
+export modules=". ${modules1}"
 echo $modules
